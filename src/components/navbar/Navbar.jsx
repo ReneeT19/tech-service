c230d1a73386c70808a5ac1a851ffc169672a74f
Renee Thomsen <rcthomsen19@gmail.com>
2022-05-25 17:39:14 -0400

    added logo in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import reTech from '../../assets/reTech.png'
 
+// BEM -> Block Element Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={reTech} alt="reTech" />
+        </div>
+      </div>
+    </div> 
   )
 }
 
