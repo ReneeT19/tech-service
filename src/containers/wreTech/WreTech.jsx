5ed181df2b435a58b492792ee5f1354a1354787c
Renee Thomsen <rcthomsen19@gmail.com>
2022-06-02 12:21:43 -0400

    set up wreTech component

--- src/containers/wreTech/WreTech.jsx
+++ src/containers/wreTech/WreTech.jsx
@@ -8,22 +8,17 @@ const WreTech = () => {
       <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
-      <div className="gpt3__whatgpt3-header">
-        <h1 className="gradient-text">
-
-        </h1>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore the service</p>
       </div>
-      {/* <div class='light x1'></div>
-         <div class='light x2'></div>
-         <div class='light x3'></div>
-         <div class='light x4'></div>
-         <div class='light x5'></div>
-         <div class='light x6'></div>
-         <div class='light x7'></div>
-         <div class='light x8'></div>
-         <div class='light x9'></div> */}
+      <div className='gpt3__whatgpt3-container'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
     </div>
   )
-}
+};
 
 export default WreTech
