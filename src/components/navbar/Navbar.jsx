3a95b28e753034ce9a2aa9d58b7953ef0c3d45d3
Renee Thomsen <rcthomsen19@gmail.com>
2022-06-07 17:14:39 -0400

    started building possibility component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -7,11 +7,11 @@ import reTech from '../../assets/reTech-logo.png';
 const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#wreTech">What is reTech?</a></p>
+    <p><a href="#wgpt3">What is reTech?</a></p>
     <p><a href="#possibility">Services</a></p>
-    <p><a href="#feature">Case Studies</a></p>
+    <p><a href="#features">Case Studies</a></p>
     <p><a href="#pricing">Pricing</a></p>
-    <p><a href="#contact">Contact</a></p>
+    <p><a href="#contact">Contact</a></p> 
   </>
 )
 // BEM -> Block Element Modifier
@@ -33,19 +33,18 @@ const Navbar = () => {
         <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
-        {toggleMenu 
-          ? <RiCloseLine color="white" size={27} onClick={() => setToggleMenu (false)} />
-          : <RiMenu3Line color="white" size={27} onClick={() => setToggleMenu (true)} />
-        }
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
         {toggleMenu && (
-          <div className="gpt3__navbar-menu_container scale-up-center">
-            <div className="gpt3__navbar-menu_container-links">
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
               <Menu />
+            </div>
               <div className="gpt3__navbar-menu_container-links-sign">
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
               </div>
-            </div>
           </div>
         )}
       </div>
@@ -60,7 +59,7 @@ const Navbar = () => {
     <div class='light x8'></div>
     <div class='light x9'></div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar
