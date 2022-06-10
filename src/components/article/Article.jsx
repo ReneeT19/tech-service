d69b930a47b26c09b544d162534b66d7d2507750
Renee Thomsen <rcthomsen19@gmail.com>
2022-06-10 11:33:22 -0400

    set up reusable component article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+//to pass different images, pass imgUrl as a prop and import different props into blog component
+const Article = ( {imgUrl, date, title }) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'> 
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Get Full Access</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './blog.css';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'; //create an imports.js under blog 
+//just like what we did for brand component
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="gpt3__blog section__padding">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening. We are blogging about it.
+        </h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date="June 10, 2022" title="This is a placeholder. This is a placeholder."/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+        <Article imgUrl={blog02} date="June 10, 2022" title="This is a placeholder. This is a placeholder."/>
+        <Article imgUrl={blog03} date="June 10, 2022" title="This is a placeholder. This is a placeholder."/>
+        <Article imgUrl={blog04} date="June 10, 2022" title="This is a placeholder. This is a placeholder."/>
+        <Article imgUrl={blog05} date="June 10, 2022" title="This is a placeholder. This is a placeholder."/>
+        </div>
+      </div>
+    </div>
   )
 }
 
