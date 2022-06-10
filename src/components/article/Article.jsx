ba0c435211c8463a0b662e5d48d9fdbd456ae1d5
Renee Thomsen <rcthomsen19@gmail.com>
2022-06-10 11:57:05 -0400

    implemented blog container

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 import './article.css';
 
 //to pass different images, pass imgUrl as a prop and import different props into blog component
-const Article = ( {imgUrl, date, title }) => {
-  return (
-    <div className='gpt3__blog-container_article'>
-      <div className='gpt3__blog-container_article-image'> 
-        <img src={imgUrl} alt="blog" />
-      </div>
-      <div className='gpt3__blog-container_article-content'>
-        <div>
-          <p>{date}</p>
-          <h3>{title}</h3>
-        </div>
-        <p>Get Full Access</p>
-      </div>
+const Article = ({ imgUrl, date, text }) => (
+  <div className="gpt3__blog-container_article">
+    <div className="gpt3__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
     </div>
-  )
-}
+    <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
 
-export default Article
+export default Article;
